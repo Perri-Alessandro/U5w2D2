97e6cbfb9159a8af48b838c15156ded090ad616f
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-04-09 21:28:59 +0200

    author image generated from server, if name and surname aren't null

--- src/main/java/perri_alessandro/U5w2D2/entities/Author.java
+++ src/main/java/perri_alessandro/U5w2D2/entities/Author.java
@@ -15,9 +15,15 @@ public class Author {
     private String cognome;
     private String mail;
     private LocalDate dataNascita;
-    private String imageUrl = "https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg";
+    private String imageUrl;
 
-
+    public void setImageUrl(String imageUrl) {
+        if (nome != null && cognome != null) {
+            this.imageUrl = String.format("https://ui-avatars.com/api/?name=%s+%s", nome, cognome);
+        } else {
+            this.imageUrl = "https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg";
+        }
+    }
 }
 
 
